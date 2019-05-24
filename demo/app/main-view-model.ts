4c526a73aa37d580babc9a31894698d5f2db97e8
Eduardo Speroni <edusperoni@gmail.com>
2019-05-24 14:29:01 -0300

    update demo

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -2,20 +2,78 @@ import { Observable } from 'tns-core-modules/data/observable';
 import { SharedNotificationDelegate } from 'nativescript-shared-notification-delegate';
 import { isIOS } from 'tns-core-modules/platform';
 
+export let invokeOnRunLoop = (function () {
+  const runloop = CFRunLoopGetMain();
+  return function (func) {
+    CFRunLoopPerformBlock(runloop, kCFRunLoopDefaultMode, func);
+    CFRunLoopWakeUp(runloop);
+  };
+}());
+
 export class HelloWorldModel extends Observable {
   public message: string;
+
+  notificationCenter: UNUserNotificationCenter;
 
   constructor() {
     super();
     if (isIOS) {
-      SharedNotificationDelegate.addObserver({
-        userNotificationCenterDidReceiveNotificationResponseWithCompletionHandler: (ncenter, resp, handler, stop, next) => {
-          console.log(ncenter, resp);
-          stop();
-        }
-      });
+      this.set("message", "initializing");
+      this.notificationCenter = UNUserNotificationCenter.currentNotificationCenter();
+      this.notificationCenter
+        .requestAuthorizationWithOptionsCompletionHandler(UNAuthorizationOptions.Alert, (granted, error) => {
+          if (!granted) {
+            this.setMessageMainLoop("notification authorization must be set");
+            return;
+          }
+          this.addObservers();
+          this.setMessageMainLoop("initialized");
+          this.scheduleNotification();
+        });
     } else {
-      console.log("This application will only work in iOS")
+      this.set("message", "This application will only work in iOS");
+      console.log("This application will only work in iOS");
     }
+  }
+
+  addObservers() {
+    SharedNotificationDelegate.addObserver({
+      userNotificationCenterWillPresentNotificationWithCompletionHandler: (ncenter, notification, handler, stop, next) => {
+        // console.log(ncenter, notification);
+        console.log("notification received by observer 1");
+        this.setMessageMainLoop(`notification received\n${notification.request.content.title}\n${notification.request.content.body}`);
+        next();
+      }
+    });
+    SharedNotificationDelegate.addObserver({
+      userNotificationCenterWillPresentNotificationWithCompletionHandler: (ncenter, notification, handler, stop, next) => {
+        // console.log(ncenter, notification);
+        console.log("notification received by observer 2 with priority 1");
+        this.setMessageMainLoop(`notification received\n${notification.request.content.title}\n${notification.request.content.body}`);
+        next();
+      }
+    }, 1);
+  }
+
+  scheduleNotification() {
+    const content = UNMutableNotificationContent.new();
+    content.title = "Test title";
+    content.body = "Test body";
+    const trigger = UNTimeIntervalNotificationTrigger.triggerWithTimeIntervalRepeats(5, false);
+    const id = "UYLLocalNotification";
+    const request = UNNotificationRequest.requestWithIdentifierContentTrigger(id, content, trigger);
+    this.notificationCenter.addNotificationRequestWithCompletionHandler(request, (e) => {
+      if (e) {
+        console.log(e);
+        return;
+      }
+      console.log("notification scheduled");
+    });
+  }
+
+  setMessageMainLoop(m: string) {
+    invokeOnRunLoop(() => {
+      this.set("message", m);
+    });
   }
 }
