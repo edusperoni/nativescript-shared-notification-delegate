4bd3665271ae1fd432a2e8808f8627964dfe4a74
Eduardo Speroni <edusperoni@gmail.com>
2019-05-24 13:25:35 -0300

    update declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,18 @@
 import { SharedNotificationDelegateCommon } from './shared-notification-delegate.common';
-export declare class SharedNotificationDelegate extends SharedNotificationDelegateCommon {
-  // define your typings manually
-  // or..
-  // take the ios or android .d.ts files and copy/paste them here
+export interface DelegateObserver {
+    userNotificationCenterDidReceiveNotificationResponseWithCompletionHandler?(center: UNUserNotificationCenter, response: UNNotificationResponse, completionHandler: () => void, stop: () => void, next: () => void): void;
+    userNotificationCenterOpenSettingsForNotification?(center: UNUserNotificationCenter, notification: UNNotification, stop: () => void, next: () => void): void;
+    userNotificationCenterWillPresentNotificationWithCompletionHandler?(center: UNUserNotificationCenter, notification: UNNotification, completionHandler: (p1: UNNotificationPresentationOptions) => void, stop: () => void, next: () => void): void;
 }
+export declare class SharedNotificationDelegateImpl extends SharedNotificationDelegateCommon {
+    _observers: Array<{
+        observer: DelegateObserver;
+        priority: number;
+    }>;
+    private delegate;
+    constructor();
+    static isUNUserNotificationCenterAvailable(): boolean;
+    addObserver(observer: DelegateObserver, priority?: number): void;
+    removeObserver(observer: DelegateObserver): void;
+}
+export declare const SharedNotificationDelegate: SharedNotificationDelegateImpl;
